5ef37759b31fe5e42bfb66aeaf0aa50299ca83eb
Nidhi Tank <135428254+Mystery008@users.noreply.github.com>
2026-05-04 12:15:07 +0530

    Fix indentation for else statement in main.py

--- main.py
+++ main.py
@@ -130,6 +130,6 @@ def categories():
 if __name__ == "__main__":
     mcp.run(transport="http", host="0.0.0.0", port=8000)
     # mcp.run()
- else:
+else:
         # Claude Desktop
         mcp.run()
